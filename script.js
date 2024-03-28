ec421356ac84c97e9171e26c50bdc3b94d3e4e90
Fernando C <thelonder461@gmail.com>
2024-03-28 19:54:03 -0300

    quase finalizando

--- script.js
+++ script.js
@@ -8,3 +8,59 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const homeLink = document.getElementById('navitems');
+    homeLink.addEventListener('click', function(event) {
+        event.preventDefault(); // Evita o comportamento padrão do link
+        location.reload(); // Recarrega a página
+    });
+});
+
+
+
+//ROLAR SLIDES AUTOMATICAMENTE//
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 7000)
+function nextImage(){
+    count++;
+    if(count>3){
+        count = 1;
+    }
+    document.getElementById("radio"+count).checked = true;
+}
+
+// Obtenha o botão e os slides
+const scrollToProdutos = document.getElementById('scrollToProdutos');
+const slides = document.querySelectorAll('.slide');
+
+// Adicione um evento de escuta para a transição dos slides
+slides.forEach(slide => {
+        slide.addEventListener('transitionstart', () => {
+            scrollToProdutos.classList.add('hidden');
+        });
+        
+        slide.addEventListener('transitionend', () => {
+            scrollToProdutos.classList.remove('hidden');
+        });
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
