2dfaf15f5846a91ba628a57467bea413837b743b
Fernando C <thelonder461@gmail.com>
2024-03-28 11:28:33 -0300

    avançando

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollToProdutos = document.getElementById('scrollToProdutos');
+    const produtosscroll = document.getElementById('produtosscroll');
 
 
+    scrollToProdutos.addEventListener('click', function() {
+        produtosscroll.scrollIntoView({ behavior: 'smooth' });
+    });
+});
 
